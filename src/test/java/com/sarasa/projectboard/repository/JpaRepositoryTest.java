cf6d64992c4e67f13526d29a199aca580197da49
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-14 12:41:46 +0900

    #6 - 데이터베이스 접근 로직 테스트 정의 3까지 완료

---
+++ src/test/java/com/sarasa/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.sarasa.projectboard.repository;
+
+import com.sarasa.projectboard.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    @DisplayName("SELECT 테스트")
+    void testSelect() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+}
