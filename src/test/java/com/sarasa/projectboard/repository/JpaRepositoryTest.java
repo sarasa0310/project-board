72d549606d64de0e8fdf38c56d90de9c13ae0a06
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-15 16:08:05 +0900

    #6 - 데이터베이스 접근 로직 테스트 정의 완료

--- src/test/java/com/sarasa/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/sarasa/projectboard/repository/JpaRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.sarasa.projectboard.repository;
 
 import com.sarasa.projectboard.config.JpaConfig;
+import com.sarasa.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -29,8 +32,60 @@ class JpaRepositoryTest {
         // Given
 
         // When
+        List<Article> articles = articleRepository.findAll();
 
         // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+    }
+
+    @Test
+    @DisplayName("INSERT 테스트")
+    void testInsert() {
+        // Given
+        long previousArticleCount = articleRepository.count();
+
+        // When
+        articleRepository.save(Article.of("spring", "spring is amazing!", "#spring"));
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount + 1);
+    }
+
+    @Test
+    @DisplayName("UPDATE 테스트")
+    void testUpdate() {
+        // Given
+        Article updatedArticle = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#spring";
+        updatedArticle.setHashtag(updatedHashtag);
+
+        // When
+        articleRepository.saveAndFlush(updatedArticle);
+
+        // Then
+        assertThat(updatedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @Test
+    @DisplayName("DELETE 테스트")
+    void testDelete() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+
+        int deletedArticleCommentSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedArticleCommentSize);
     }
 
 }
