eef62d764bf2c1f36f48f759a3c6e74bcb6fb255
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-16 09:59:37 +0900

    #4 - Spring Data REST 설정

--- src/main/java/com/sarasa/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/sarasa/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.sarasa.projectboard.repository;
 
 import com.sarasa.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
