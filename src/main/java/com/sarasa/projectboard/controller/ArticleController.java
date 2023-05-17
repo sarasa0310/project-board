c20fffd46f8b19a0d075dd491427f541300c54b1
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-05-17 11:33:19 +0900

    #8 - 게시글 뷰 기본 골격 작성

--- src/main/java/com/sarasa/projectboard/controller/ArticleController.java
+++ src/main/java/com/sarasa/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.sarasa.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,7 +15,17 @@ public class ArticleController {
     @GetMapping
     public String getArticles(ModelMap map) {
         map.addAttribute("articles", List.of());
+
         return "articles/index";
+    }
+
+    @GetMapping("/{article-id}")
+    public String getOneArticle(ModelMap map,
+                                @PathVariable("article-id") Long articleId) {
+        map.addAttribute("article", "articleValue"); // todo: 실제 데이터로 교체 필요
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 
 }
--- src/test/java/com/sarasa/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/sarasa/projectboard/controller/ArticleControllerTest.java
@@ -34,7 +34,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[VIEW] [GET] 게시글 상세 페이지")
     @Test
     public void getOneArticleTest() throws Exception {
