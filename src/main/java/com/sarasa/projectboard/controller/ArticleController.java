edf7d20a5c7abcb50a5378392e75741c4ed85afb
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-02 21:48:14 +0900

    #9 - ArticleController 구현
    - ArticleController 테스트 코드 업데이트
    - 업데이트 한 테스트 코드에 맞게 컨트롤러 구현

--- src/main/java/com/sarasa/projectboard/controller/ArticleController.java
+++ src/main/java/com/sarasa/projectboard/controller/ArticleController.java
@@ -1,20 +1,38 @@
 package com.sarasa.projectboard.controller;
 
+import com.sarasa.projectboard.domain.type.SearchType;
+import com.sarasa.projectboard.response.ArticleResponse;
+import com.sarasa.projectboard.response.ArticleWithCommentsResponse;
+import com.sarasa.projectboard.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/articles")
+@RequiredArgsConstructor
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String getArticles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String getArticles(@RequestParam(required = false) SearchType searchType,
+                              @RequestParam(required = false) String keyword,
+                              @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                              ModelMap map) {
+        Page<ArticleResponse> articleResponsePage =
+                articleService.searchArticles(searchType, keyword, pageable)
+                        .map(ArticleResponse::from);
+
+        map.addAttribute("articles", articleResponsePage);
 
         return "articles/index";
     }
@@ -22,8 +40,11 @@ public class ArticleController {
     @GetMapping("/{article-id}")
     public String getOneArticle(ModelMap map,
                                 @PathVariable("article-id") Long articleId) {
-        map.addAttribute("article", "articleValue"); // todo: 실제 데이터로 교체 필요
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article =
+                ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentResponses());
 
         return "articles/detail";
     }
