b0d2fc48e5837d46968ee5f685979512a2d48d78
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-06 12:27:29 +0900

    Fix: css 파일 정확한 위치로 이동

--- src/main/java/com/sarasa/projectboard/response/ArticleResponse.java
+++ src/main/java/com/sarasa/projectboard/response/ArticleResponse.java
@@ -20,6 +20,7 @@ public record ArticleResponse(
 
     public static ArticleResponse from(ArticleDto dto) {
         String nickname = dto.userAccountDto().nickname();
+
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountDto().username();
         }
