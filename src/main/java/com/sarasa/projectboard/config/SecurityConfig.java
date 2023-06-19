163d320873b25c898b4904d9fcf45199561d1559
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-19 23:18:47 +0900

    #12 - Spring Security 설정 업데이트

--- src/main/java/com/sarasa/projectboard/config/SecurityConfig.java
+++ src/main/java/com/sarasa/projectboard/config/SecurityConfig.java
@@ -1,20 +1,52 @@
 package com.sarasa.projectboard.config;
 
+import com.sarasa.projectboard.dto.UserAccountDto;
+import com.sarasa.projectboard.dto.security.BoardPrincipal;
+import com.sarasa.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin();
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 검사에서 정적 리소스 제외
+                        .mvcMatchers(HttpMethod.GET,
+                                "/", "/articles", "/articles/search-hashtag").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/");
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "유저를 찾을 수 없습니다 -> username: " + username
+                ));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
